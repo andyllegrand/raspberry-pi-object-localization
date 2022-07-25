ed7facf26a876a4c89378b0045698b8104a87827
andylegrand <andyleg@uw.edu>
2022-07-25 15:54:37 -0700

    still a work in progress. individual modules work just need to put them together

---
+++ findCameraIntrensics.py
@@ -0,0 +1,43 @@
+import os
+
+import numpy as np
+import cv2 as cv
+import glob
+import pickle
+os.chdir("/Users/andylegrand/PycharmProjects/localization_image_testing/camera1")
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(7,7,0)
+objp = np.zeros((7*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('*.jpg')
+for fname in images:
+    print(fname)
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7,7), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        print("works")
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,7), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+    else:
+        os.remove(fname)
+        print("removed")
+cv.destroyAllWindows()
+
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+print(mtx)
+print(dist)
+with open('cam1Params', 'wb') as f:
+    pickle.dump([mtx, dist], f)
